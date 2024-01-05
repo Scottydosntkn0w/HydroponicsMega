4acca929528b7b7e3bcbb69e0171a03630c8bdb2
shendricks <shendricks@bensonhill.com>
2024-01-05 11:56:08 -0600

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,25 @@
+#include <Arduino.h>
+
+#define LED_PIN 13
+#define BLINK_DELAY 100
+
+int n = 0;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(LED_PIN, OUTPUT);
+}
+
+void loop() {
+
+
+
+  digitalWrite(LED_PIN,HIGH);
+  delay(BLINK_DELAY);
+  digitalWrite(LED_PIN, LOW);
+  delay(BLINK_DELAY);
+
+  Serial.println(sin(n*2*3.1416/20));
+  n++;
+
+}
